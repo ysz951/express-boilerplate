4a21459d23e47ae62721f991affc378fa9ad0e96
ysz951 <shengyang_zhou@baylor.edu>
2020-07-16 04:23:37 -0500

    change message

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(helmet())
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hello, world')
+    res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
